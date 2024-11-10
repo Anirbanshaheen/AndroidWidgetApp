96d4d4c09a668beddf99cb320926ba7736feaf41
Anirbanshaheen <anirbanshaheen97@gmail.com>
2024-11-10 11:04:26 +0600

    todo sample commit

--- app/src/main/java/com/example/androidwidgetapp/MainActivity.kt
+++ app/src/main/java/com/example/androidwidgetapp/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        init()
+        init() // todo commit
     }
 
     private fun init(){
